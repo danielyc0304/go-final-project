6675a45f8e68a01e3e968eaa2e547f3c999be45d
Nick Chang <nick.ee12@nycu.edu.tw>
2025-12-06 23:40:26 +0800

    revert: cancel change of backend files

--- backend/main.go
+++ backend/main.go
@@ -7,8 +7,6 @@ import (
 	"backend/services"
 
 	beego "github.com/beego/beego/v2/server/web"
-	"github.com/beego/beego/v2/server/web/filter/cors"
-	
 )
 
 func init() {
@@ -18,14 +16,6 @@ func init() {
 
 	// 啟動限價單撮合服務
 	services.GlobalLimitOrderMatcher.Start()
-
-	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-	}))
 }
 
 func main() {
